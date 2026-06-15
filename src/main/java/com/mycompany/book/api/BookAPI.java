0fbbbb39b562491675d11f8ef8b38f040e69da9e
HaikalCannavaro <haikalcannavaro64@gmail.com>
2026-06-15 20:59:48 +0700

    Initial Java Maven project

---
+++ src/main/java/com/mycompany/book/api/BookAPI.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.book.api;
+
+
+public class BookAPI {
+    public static String getBook(){
+        return "Book API Running";
+    }
+
+    public static void main(String[] args){
+        System.out.println(getBook());
+    }
+}
